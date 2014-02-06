45bd9f040470267205812351068286b9d4c32593
Lily Sierra <lilys@stanford.edu>
2014-02-05 22:26:08 -0800

    added copy of everything

---
+++ js/login.js
@@ -0,0 +1,23 @@
+'use strict';
+//Call this function when the page loads (the "ready event")
+
+$(document).ready(function() {
+	initializePage();
+})
+
+function initializePage() {
+	$("#loginBtn").click(loginCheck);
+}
+
+function loginCheck(e) {
+	var username = $('#username').val();
+	console.log("username is" + username);
+	var password = $('#password').val();
+	if (username.length == 0 || password.length == 0) {
+		$("#loginInstructions").text("Please enter a username and password.");
+	} else {
+		window.location.href = ("index.html");
+		$(".toolbar p").text(username);
+		
+	}
+}
